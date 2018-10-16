81268841de447c1b4f74684891c97b8a3cbf2946
s-pace <sylvain.pace@algolia.com>
2018-10-16 18:39:00 +0200

    sen email

--- index.js
+++ index.js
@@ -1,94 +1,101 @@
 "use strict";
 
-var request = require('request');
-'use strict';
-const nodemailer = require('nodemailer');
-const cheerio = require('cheerio')
+var request = require("request");
+("use strict");
+const nodemailer = require("nodemailer");
+const cheerio = require("cheerio");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const departureTime = process.env.DEPARTURE_TIME
-const arrivalTime = process.env.ARRIVAL_TIME
-const departure = process.env.DEPARTURE
-const arrival = process.env.ARRIVAL
+const departureTime = process.env.DEPARTURE_TIME;
+const arrivalTime = process.env.ARRIVAL_TIME;
+const departure = process.env.DEPARTURE;
+const arrival = process.env.ARRIVAL;
 
-console.log(`Request for ${departure} to ${arrival} departureTime:${departureTime} arrivalTime:${arrivalTime}`)
+const formatedDepartureTime = new Date(departureTime)
+const formatedArrivalTime = new Date(arrivalTime)
 
+const constraint = `${departure} to ${arrival} at ${formatedDepartureTime.toString()} until ${formatedArrivalTime.toString()} `
+console.log(`${constraint}\n\n\n`)
 
 request("https://simulateur.tgvmax.fr/VSC/", (error, response, html) => {
-
     if (!error && response.statusCode == 200) {
         var $ = cheerio.load(html);
-        const hiddenToken = ($("#hiddenToken").val())
-        console.log(`hiddenToken=${hiddenToken}`)
+        const hiddenToken = $("#hiddenToken").val();
+        console.log(`hiddenToken=${hiddenToken}`);
         const options = {
             url: `https://sncf-simulateur-api-prod.azurewebsites.net/api/RailAvailability/Search/${departure}/${arrival}/${departureTime}/${arrivalTime}`,
             headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Referer': 'https://simulateur.tgvmax.fr/VSC/',
-                'Origin': 'https://simulateur.tgvmax.fr',
-                'Content-Type': 'application/json',
-                'ValidityToken': hiddenToken,
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+                Accept: "application/json, text/plain, */*",
+                Referer: "https://simulateur.tgvmax.fr/VSC/",
+                Origin: "https://simulateur.tgvmax.fr",
+                "Content-Type": "application/json",
+                ValidityToken: hiddenToken,
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
             },
             json: true
         };
 
-
-        function callback(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                const nbTrains = body.length
-                const avaialbleTrains = body.filter(t => t.availableSeatsCount > 0)
+        function callback(error2, response2, body2) {
+            if (!error2 && response2.statusCode == 200) {
+                const nbTrains = body2.length;
+                console.log(body2)
+                const avaialbleTrains = body2.filter(t => t.availableSeatsCount > 0);
                 if (avaialbleTrains.length > 0) {
                     console.log("alleluja");
-                    avaialbleTrains.map(console.log)
+                    avaialbleTrains.map(console.log);
+                    // Generate test SMTP service account from ethereal.email
+                    // Only needed if you don't have a real mail account for testing
+                    nodemailer.createTestAccount((err, account) => {
+                        // create reusable transporter object using the default SMTP transport
+
+                        const host = process.env.SMTP_SERVER;
+                        const port = process.env.SMTP_PORT;
+                        const user = process.env.SMTP_USER;
+                        const pass = process.env.SMTP_PASSWORD;
+                        const receiver = process.env.RECEIVER;
+                        const sender = process.env.SENDER;
+
+                        let transporter = nodemailer.createTransport({
+                            host: host,
+                            port: port,
+                            secure: true, // true for 465, false for other ports
+                            auth: {
+                                user: user, // generated ethereal user
+                                pass: pass // generated ethereal password
+                            }
+                        });
+                        const traintText = avaialbleTrains.reduce((acc, train) => `${acc}\n\n Train:${train.train} at ${train.departureDateTime} availableSeatsCount:${train.availableSeatsCount}`, '')
+                        const text = `We have found trains 🎉\n ${traintText}`
+                        // setup email data with unicode symbols
+                        let mailOptions = {
+                            from: `"TGV max robot 🤖" <${sender}>`, // sender address
+                            to: receiver, // list of receivers
+                            subject: ` ${constraint} found `, // Subject line
+                            text: text, // plain text body
+                            html: text.split('\n').join('\n<br>\n') // html body
+                        };
+
+                        // send mail with defined transport object
+                        transporter.sendMail(mailOptions, (e, info) => {
+                            if (e) {
+                                return console.error(e);
+                            }
+                            console.log("Message sent: %s", info.messageId);
+                        });
+                    });
                 } else {
                     console.log(`Next time ;), not found over ${nbTrains} trains`);
-
                 }
-                // // Generate test SMTP service account from ethereal.email
-                // // Only needed if you don't have a real mail account for testing
-                // nodemailer.createTestAccount((err, account) => {
-                //     // create reusable transporter object using the default SMTP transport
-                //     let transporter = nodemailer.createTransport({
-                //         host: 'smtp.ethereal.email',
-                //         port: 587,
-                //         secure: false, // true for 465, false for other ports
-                //         auth: {
-                //             user: account.user, // generated ethereal user
-                //             pass: account.pass // generated ethereal password
-                //         }
-                //     });
 
-                //     // setup email data with unicode symbols
-                //     let mailOptions = {
-                //         from: '"Fred Foo 👻" <foo@example.com>', // sender address
-                //         to: 'bar@example.com, baz@example.com', // list of receivers
-                //         subject: 'Hello ✔', // Subject line
-                //         text: 'Hello world?', // plain text body
-                //         html: '<b>Hello world?</b>' // html body
-                //     };
-
-                //     // send mail with defined transport object
-                //     transporter.sendMail(mailOptions, (error, info) => {
-                //         if (error) {
-                //             return console.error(error);
-                //         }
-                //         console.log('Message sent: %s', info.messageId);
-                //         // Preview only available when sending through an Ethereal account
-                //         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-                //         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-                //         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-                //     });
-                // });
             } else {
-                console.error(response)
-
+                console.error(`Status: ${response2.status}`);
+                console.error(body2);
+                console.error(error2);
+                console.error(response2);
             }
         }
 
         request(options, callback);
     }
-
 });
