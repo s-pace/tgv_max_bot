42ac2b515f6f2f8d6337c1aca42ffd51bda828c3
s-pace <sylvain.pace@algolia.com>
2019-04-18 21:09:27 +0200

    update with chalk and better refactor

--- book.js
+++ book.js
@@ -1,14 +1,15 @@
 const fetch = require("node-fetch");
-
+const chalk = require('chalk');
 
 require("dotenv").config();
 
 const email = process.env.TRAINLINE_EMAIL;
 const password = process.env.TRAINLINE_PASSWORD;
 
-console.log({
+console.info(chalk.cyan("Looking a train for " +
     email
-})
+))
+
 extractCookieString = (r) => r.substring(r.indexOf("=") + 1, r.indexOf(";"))
 
 stringifyCookie = c => Object.keys(c).reduce((acc, e) => {
@@ -18,8 +19,9 @@ stringifyCookie = c => Object.keys(c).reduce((acc, e) => {
     return acc + e + "=" + c[e]
 }, '')
 
-signIn = (url, cookie) => {
+buildRequest = (url, body, cookie, token, extraHeaders) => {
 
+    const referrer = url.split("/").pop();
     let opts = {
         credentials: 'include',
         "headers": {
@@ -31,15 +33,15 @@ signIn = (url, cookie) => {
             "x-ct-client-id": "761c18b4-14ec-44a7-bd33-61100dd7faab",
             "x-ct-locale": "fr",
             "x-ct-timestamp": "1554970536",
-            "x-ct-version": "1254b1d98ce9b91e3e5b46a3c6e7c159e5990497",
+            // "x-ct-version": "1254b1d98ce9b91e3e5b46a3c6e7c159e5990497",
             "x-not-a-bot": "i-am-human",
             "x-requested-with": "XMLHttpRequest",
             "x-user-agent": "CaptainTrain/1554970536(web) (Ember 3.5.1)",
             "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
         },
-        "referrer": "https://www.trainline.fr/signin",
+        "referrer": "https://www.trainline.fr/" + referrer,
         "referrerPolicy": "no-referrer-when-downgrade",
-        "body": "{\"id\":\"1\",\"email\":\"" + email + "\",\"password\":\"" + password + "\",\"facebook_id\":null,\"facebook_token\":null,\"google_code\":null,\"concur_auth_code\":null,\"concur_new_email\":null,\"concur_migration_type\":null,\"source\":null,\"correlation_key\":null,\"auth_token\":null,\"user_id\":null}",
+        body,
         "method": "POST",
         "mode": "cors"
     }
@@ -50,60 +52,54 @@ signIn = (url, cookie) => {
             })
         })
     }
+    if (extraHeaders) {
+        opts = Object.assign(opts, {
+            headers: Object.assign(opts.headers, extraHeaders)
+        })
+    }
+
+    if (token) {
+        opts = Object.assign(opts, {
+            headers: Object.assign(opts.headers, {
+                "authorization": "Token token=" + token,
+            })
+        })
+    }
+    console.info(url)
+    console.info(opts)
     return fetch(url, opts)
 }
 
 const getToken = async url => {
     try {
-        const signInResponse = await signIn(url)
+        bodyLogin = "{\"id\":\"1\",\"email\":\"" + email + "\",\"password\":\"" + password + "\",\"facebook_id\":null,\"facebook_token\":null,\"google_code\":null,\"concur_auth_code\":null,\"concur_new_email\":null,\"concur_migration_type\":null,\"source\":null,\"correlation_key\":null,\"auth_token\":null,\"user_id\":null}"
+        const signInResponse = await buildRequest(url, bodyLogin)
         const ak_bmsc = extractCookieString(signInResponse.headers.get('set-cookie'));
+        console.info(chalk.bgBlue("First signin responsed with a status: " + signInResponse.status))
 
-        const resp2ndtoken = await signIn(url, {
+        const resp2ndtoken = await buildRequest(url, bodyLogin, {
             ak_bmsc
         })
+        console.info(chalk.bgBlue("2nd signin responsed with a status: " + resp2ndtoken.status))
 
         const bm_sv = extractCookieString(resp2ndtoken.headers.get('set-cookie'));
 
         const jsonSignin = await resp2ndtoken.json();
         const token = jsonSignin.meta.token
-        console.info("Token & cookie fetched")
-        console.info({
-            "authorization": "Token token=" + token
-        })
-        console.info({
+        console.info(chalk.yellow("Token & cookie fetched"))
+        console.info(chalk.yellow(JSON.stringify({
+            "authorization": "Token token=" + token,
             ak_bmsc,
             bm_sv,
-        })
+        }, null, 4)))
 
-        const search = await fetch("https://www.trainline.fr/api/v5_1/search", {
-            "credentials": "include",
-            "headers": {
-                "accept": "application/json, text/javascript, */*; q=0.01",
-                "accept-language": "fr-FR,fr;q=0.8",
-                "authorization": "Token token=" + token,
-                "cache-control": "no-cache",
-                "content-type": "application/json; charset=UTF-8",
-                "pragma": "no-cache",
-                "x-ct-client-id": "761c18b4-14ec-44a7-bd33-61100dd7faab",
-                "x-ct-locale": "fr",
-                "x-ct-timestamp": "1554970536",
-                "x-ct-version": "1254b1d98ce9b91e3e5b46a3c6e7c159e5990497",
-                "x-not-a-bot": "i-am-human",
-                "x-requested-with": "XMLHttpRequest",
-                "x-user-agent": "CaptainTrain/1554970536(web) (Ember 3.5.1)",
-                "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
-                cookie: stringifyCookie({
-                    ak_bmsc,
-                    bm_sv
-                })
-            },
-            "referrer": "https://www.trainline.fr/search",
-            "referrerPolicy": "no-referrer-when-downgrade",
-            "body": "{\"search\":{\"departure_date\":\"2019-04-25T13:00:00UTC\",\"arrival_date\":\"2019-04-25T18:20:00UTC\",\"systems\":[\"sncf\"],\"departure_station_id\":\"4916\",\"arrival_station_id\":\"4718\",\"passenger_ids\":[\"34135937\"],\"card_ids\":[\"1833434\",\"12669735\"]}}",
-            "method": "POST",
-            "mode": "cors"
-        });
-        const searchJson = await search.json("https://www.trainline.fr/api/v5_1/account/signin");
+        const search = await buildRequest("https://www.trainline.fr/api/v5_1/search", "{\"search\":{\"departure_date\":\"2019-04-25T13:00:00UTC\",\"arrival_date\":\"2019-04-25T18:20:00UTC\",\"systems\":[\"sncf\"],\"departure_station_id\":\"4916\",\"arrival_station_id\":\"4718\",\"passenger_ids\":[\"34135937\"],\"card_ids\":[\"1833434\",\"12669735\"]}}", {
+            ak_bmsc,
+            bm_sv
+        }, token)
+        console.info(chalk.bgBlue("Search responsed with a status: " + search.status))
+
+        const searchJson = await search.json();
 
         const trips = searchJson.trips
         const freeFolder = searchJson.folders.filter(f => f.cents == 0)
@@ -123,36 +119,8 @@ const getToken = async url => {
             mobile: "no",
             bm_sv
         }
-        const stringCookie = stringifyCookie(bookCookie)
 
-        bookOpts = {
-            "credentials": "include",
-            "headers": {
-                "accept": "application/json, text/javascript, */*; q=0.01",
-                "accept-language": "fr-FR,fr;q=0.8",
-                "cache-control": "no-cache",
-                "content-type": "application/json; charset=UTF-8",
-                "pragma": "no-cache",
-                "x-ct-client-id": "761c18b4-14ec-44a7-bd33-61100dd7faab",
-                "x-ct-locale": "fr",
-                "x-ct-timestamp": "1554970536",
-                "x-ct-version": "1254b1d98ce9b91e3e5b46a3c6e7c159e5990497",
-                "x-not-a-bot": "i-am-human",
-                "x-requested-with": "XMLHttpRequest",
-                "x-user-agent": "CaptainTrain/1554970536(web) (Ember 3.5.1)",
-                "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
-                "authorization": "Token token=" + token,
-                "cache-control": "no-cache",
-                "cookie": stringCookie
-            },
-            "referrer": "https://www.trainline.fr/search/paris/lyon/2019-04-25-12:00",
-            "referrerPolicy": "no-referrer-when-downgrade",
-            "body": "{\"book\":{\"search_id\":\"" + folderToBook.search_id + "\",\"outward_folder_id\":\"" + folderToBook.id + "\",\"options\":{\"" + tripToBook.segment_ids[0] + "\":{\"comfort_class\":\"pao.default\",\"seat\":\"aisle\"}}}}",
-            "method": "POST",
-            "mode": "cors"
-        }
-
-        const book = await fetch("https://www.trainline.fr/api/v5_1/book", bookOpts);
+        const book = await buildRequest("https://www.trainline.fr/api/v5_1/book", "{\"book\":{\"search_id\":\"" + folderToBook.search_id + "\",\"outward_folder_id\":\"" + folderToBook.id + "\",\"options\":{\"" + tripToBook.segment_ids[0] + "\":{\"comfort_class\":\"pao.default\",\"seat\":\"aisle\"}}}}", bookCookie, token);
         console.log(book)
 
     } catch (error) {
